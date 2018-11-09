11b5902ee805a5c8307e09693379530891348278
Matteo Bertozzo <matteobertozzo@outlook.com>
2018-11-09 17:26:33 +0100

    Setup local WS server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,30 @@
-const express = require('express');
-const os = require('os');
+var express = require('express');
+var http = require('http');
+var WebSocket = require('ws');
 
-const app = express();
+var app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+//initialize a simple http server
+var server = http.createServer(app);
+
+//initialize the WebSocket server instance
+var wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
